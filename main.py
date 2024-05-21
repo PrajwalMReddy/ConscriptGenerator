70a3127a1871f5c0b85915f1c2861dda950428a4
PrajwalMReddy <48818107+PrajwalMReddy@users.noreply.github.com>
2024-05-21 18:09:52 +0530

    Add Image Generation

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from PIL import Image
+
+unit = 50
+
+
+# TODO
+def find_text_width(text):
+    return unit * len(text)  # Temporary Code
+
+
 def main():
-    pass
+    text_to_render = input("Enter Conlang Text: ")
+    text_width = find_text_width(text_to_render)
+
+    image = Image.new(mode="RGB", size=(text_width, unit * 3), color=(255, 255, 255))
+    image.show()
 
 
 if __name__ == '__main__':
